6c2b7f7b1d89612f897cd36485b0eddea5a3ba81
tim tucker <timothytucker@berkeley.edu>
2021-03-03 13:22:41 -0800

    changes to personal website, added dev playground, added proof of concept parallax to playground

--- filmfest-concept/js/script.js
+++ filmfest-concept/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $('#main').fadeIn(1500);
+  randomEffect();
 })
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 
 $(window).scroll(function() {
 	parallax();
@@ -8,7 +17,16 @@ $(window).scroll(function() {
 
 function parallax() {
 	var wScroll = $(window).scrollTop();
-	$('.down-indicator').css('opacity', (100 - (wScroll * 0.4)) + '%');
+	$('.down-indicator').css('opacity', (100 - (wScroll * 0.5)) + '%');
 	$('.parallax-bg').css('background-position', 'center ' + (10 + (wScroll * 0.05)) + 'em');
-	$('.parallax-bg-main').css('background-position', 'center ' + (10 - (wScroll * 0.002)) + 'em');
+	$('.parallax-bg-main').css('background-position', 'center ' + (10 - (wScroll * 0.0002)) + 'em');
+}
+
+/* animation: bobble 1s infinite alternate; */
+function randomEffect() {
+  thumbnailArray = $('.thumbnail');
+  thumbnailArray.each(function(index) {
+    var scale = Math.floor(Math.random() * Math.floor(2000));
+    $(this).css('animation', 'bobble 5s infinite ' + scale + 'ms alternate');
+  })
 }
