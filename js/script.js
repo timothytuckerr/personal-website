4ea0c8a488df249ad84395b21ad99b2dc0115f30
tim tucker <timothytucker@berkeley.edu>
2021-03-03 22:56:28 -0800

    updated textures, changed navigation bar, added scroll animation smoothing

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#main').fadeIn(2300);
+})
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
--- script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $('#main').fadeIn(2300);
-})
